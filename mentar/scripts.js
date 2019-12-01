9ab2eeaef55e2b55150ab4698a28884b0804b564
Dmitry Molchanov <dmitriy.molchanov13@gmail.com>
2019-12-02 00:15:29 +0300

    Normal work of js

--- mentar/scripts.js
+++ mentar/scripts.js
@@ -7,68 +7,69 @@ let result;
 let start_time;
 
 
+function generate_problem(){
+	let txt_first_operand = document.getElementById('txtFirstOperand');
+	let txt_second_operand = document.getElementById('txtSecondOperand');
+
+	// Две следующие переменные можно сделать сразу же значениями (.value)
+	let slt_first_operand = document.getElementById('sltDigitNumber_1'); 
+	let slt_second_operand = document.getElementById('sltDigitNumber_2');
+	
+	let first_operand = Math.round(
+		10**(+slt_first_operand.value - 1) + Math.random() * ((10**(+slt_first_operand.value) - 1) - 10**(+slt_first_operand.value - 1))
+	);
+	let second_operand = Math.round(
+		10**(+slt_second_operand.value - 1) + Math.random() * ((10**(+slt_second_operand.value) - 1) - 10**(+slt_second_operand.value - 1))
+	);
+
+	txt_first_operand.value = first_operand;
+	txt_second_operand.value = second_operand;
+
+	let txt_operator = document.getElementById('txtOperator'); 
+	let slt_operator = document.getElementById('sltOperator');
+
+	let used_operators = [];
+	for (let i = 0; i < 3; i++) {
+		let chbox = document.getElementById('cbxOp' + i) 
+		if (chbox.checked){
+			used_operators.push(chbox.value);
+		}
+	}
+
+	// !Здесь нужно добавить проверку на то, что выбран хоть один чекбокс!
+
+	operator_index = Math.round(Math.random()*(used_operators.length - 1));
+
+	switch (used_operators[operator_index]) {
+		case '+':
+			txt_operator.value = '+';
+			result = first_operand + second_operand;
+			break;
+		case '-':
+			txt_operator.value = '-';
+			result = first_operand - second_operand;
+			break;
+		case 'x':
+			txt_operator.value = 'x';
+			result = first_operand * second_operand;
+			break;
+	}
+
+	let txt_result = document.getElementById('txtResult');
+	txt_result.value = '';
+
+	txt_result.focus();
+	start_time = performance.now();
+}
+
 // btn_generate.addEventListener('click', function(){
 btn_generate.addEventListener('keyup', function(e){
-		let first_operand = document.getElementById('txtFirstOperand');
-		let second_operand = document.getElementById('txtSecondOperand');
-		let slt_first_operand = document.getElementById('sltDigitNumber_1');
-		let slt_second_operand = document.getElementById('sltDigitNumber_2');
-		
-		first_operand.value = Math.round(
-			10**(+slt_first_operand.value - 1) + Math.random() * ((10**(+slt_first_operand.value) - 1) - 10**(+slt_first_operand.value - 1))
-		);
-		second_operand.value = Math.round(
-			10**(+slt_second_operand.value - 1) + Math.random() * ((10**(+slt_second_operand.value) - 1) - 10**(+slt_second_operand.value - 1))
-		);
-
-		let operator = document.getElementById('txtOperator'); 
-		let slt_operator = document.getElementById('sltOperator');
-		switch (slt_operator.value) {
-			case 'r':
-				k = Math.round(1 + Math.random() * 2);
-				switch (k) {
-					case 1:
-						operator.value = '+';
-						break;
-					case 2:
-						operator.value = '-';
-						break;
-					case 3:
-						operator.value = 'x';
-						break;
-				}
-				break; 
-			case '+':
-				operator.value = '+';
-				break;
-			case '-':
-				operator.value = '-';
-				break;
-			case 'x':
-				operator.value = 'x';
-				break;
-		}
-
-		switch (operator.value) {
-			case '+':
-				result = +first_operand.value + +second_operand.value;
-				break;
-			case '-':
-				result = +first_operand.value - +second_operand.value;
-				break;
-			case 'x':
-				result = +first_operand.value * +second_operand.value;
-				break;
-		}
-
-		let txt_result = document.getElementById('txtResult');
-		txt_result.value = '';
-
-		txt_result.focus();
-		start_time = performance.now();
+	generate_problem();		
 });
 
-
+btn_generate.addEventListener('click', function(){
+	generate_problem();
+});
 
 btn_submit.addEventListener('click', function(){
 	calculation_time = performance.now() - start_time;
@@ -107,6 +108,7 @@ txt_result.addEventListener('keyup', function(e){
 // !Добавить защиту от дурака: сначала нажать submit без предварительного нажатия btn_generate
 // !Проверять, ввел ли пользователь вообще какое-либо число
 // !Switch'и ужасны!
+// !27.11.2019 - неадекватно работают нажатия на кнопки (различается поведение при вводе с клавиатуры и при нажатии мышкой)
 
 // !Проверить по поводу анонимности функций
 // (function() {
