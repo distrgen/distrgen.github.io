b331a98398f2e595da5423482d75366572b1d744
Dmitry Molchanov <dmitriy.molchanov13@gmail.com>
2019-12-02 15:18:28 +0300

    I guess that it works... Change click to mouseup

--- mentar/scripts.js
+++ mentar/scripts.js
@@ -8,70 +8,64 @@ let start_time;
 
 
 function generate_problem(){
-	let txt_first_operand = document.getElementById('txtFirstOperand');
-	let txt_second_operand = document.getElementById('txtSecondOperand');
-
-	// Две следующие переменные можно сделать сразу же значениями (.value)
-	let slt_first_operand = document.getElementById('sltDigitNumber_1'); 
-	let slt_second_operand = document.getElementById('sltDigitNumber_2');
-	
-	let first_operand = Math.round(
-		10**(+slt_first_operand.value - 1) + Math.random() * ((10**(+slt_first_operand.value) - 1) - 10**(+slt_first_operand.value - 1))
-	);
-	let second_operand = Math.round(
-		10**(+slt_second_operand.value - 1) + Math.random() * ((10**(+slt_second_operand.value) - 1) - 10**(+slt_second_operand.value - 1))
-	);
-
-	txt_first_operand.value = first_operand;
-	txt_second_operand.value = second_operand;
-
-	let txt_operator = document.getElementById('txtOperator'); 
-	let slt_operator = document.getElementById('sltOperator');
-
+	let no_checkbox_is_checked = true;
 	let used_operators = [];
 	for (let i = 0; i < 3; i++) {
 		let chbox = document.getElementById('cbxOp' + i) 
 		if (chbox.checked){
 			used_operators.push(chbox.value);
+			no_checkbox_is_checked = false;
 		}
 	}
+	if (no_checkbox_is_checked) {
+		alert('Please, select any operator.');
+	} else {
+		let txt_first_operand = document.getElementById('txtFirstOperand');
+		let txt_second_operand = document.getElementById('txtSecondOperand');
 
-	// !Здесь нужно добавить проверку на то, что выбран хоть один чекбокс!
+		// Две следующие переменные можно сделать сразу же значениями (.value)
+		let slt_first_operand = document.getElementById('sltDigitNumber_1'); 
+		let slt_second_operand = document.getElementById('sltDigitNumber_2');
+		
+		let first_operand = Math.round(
+			10**(+slt_first_operand.value - 1) + Math.random() * ((10**(+slt_first_operand.value) - 1) - 10**(+slt_first_operand.value - 1))
+		);
+		let second_operand = Math.round(
+			10**(+slt_second_operand.value - 1) + Math.random() * ((10**(+slt_second_operand.value) - 1) - 10**(+slt_second_operand.value - 1))
+		);
 
-	operator_index = Math.round(Math.random()*(used_operators.length - 1));
+		txt_first_operand.value = first_operand;
+		txt_second_operand.value = second_operand;
 
-	switch (used_operators[operator_index]) {
-		case '+':
-			txt_operator.value = '+';
-			result = first_operand + second_operand;
-			break;
-		case '-':
-			txt_operator.value = '-';
-			result = first_operand - second_operand;
-			break;
-		case 'x':
-			txt_operator.value = 'x';
-			result = first_operand * second_operand;
-			break;
+		let txt_operator = document.getElementById('txtOperator'); 
+		let slt_operator = document.getElementById('sltOperator');
+		
+		operator_index = Math.round(Math.random()*(used_operators.length - 1));
+
+		switch (used_operators[operator_index]) {
+			case '+':
+				txt_operator.value = '+';
+				result = first_operand + second_operand;
+				break;
+			case '-':
+				txt_operator.value = '-';
+				result = first_operand - second_operand;
+				break;
+			case 'x':
+				txt_operator.value = 'x';
+				result = first_operand * second_operand;
+				break;
+		}
+
+		let txt_result = document.getElementById('txtResult');
+		txt_result.value = '';
+
+		txt_result.focus();
+		start_time = performance.now();
 	}
-
-	let txt_result = document.getElementById('txtResult');
-	txt_result.value = '';
-
-	txt_result.focus();
-	start_time = performance.now();
 }
 
-// btn_generate.addEventListener('click', function(){
-btn_generate.addEventListener('keyup', function(e){
-	generate_problem();		
-});
-
-btn_generate.addEventListener('click', function(){
-	generate_problem();
-});
-
-btn_submit.addEventListener('click', function(){
+function submit_answer(){
 	calculation_time = performance.now() - start_time;
 	let txt_result = document.getElementById('txtResult');
 	let user_result = +txt_result.value;
@@ -84,10 +78,30 @@ btn_submit.addEventListener('click', function(){
 	document.getElementById('pTime').innerHTML = Math.round(calculation_time) + ' ms';	
 	document.getElementById('btnGenerate').focus();
 	txt_result.blur();
+}
 
+// btn_generate.addEventListener('click', function(){
+btn_generate.addEventListener('keyup', function(e){
+	if (e.code === 'Enter') {
+		generate_problem();		
+	}
 });
 
-btn_options.addEventListener('click', function(){
+btn_generate.addEventListener('mouseup', function(){
+	generate_problem();
+});
+
+btn_submit.addEventListener('mouseup', function(){
+	submit_answer();
+});
+
+btn_submit.addEventListener('keyup', function(e){
+	if (e.code === 'Enter') {
+		submit_answer();
+	}
+});
+
+btn_options.addEventListener('mouseup', function(){
 	let stn_options = document.getElementById('stnOptions');
 	if (stn_options.style.display == 'none' || stn_options.style.display =='') {
 		stn_options.style.display = 'flex';
@@ -97,10 +111,13 @@ btn_options.addEventListener('click', function(){
 	}
 });
 
+// Надо вынести код кнопки submit в отдельную функцию
+
 txt_result.addEventListener('keyup', function(e){
 	if (e.code === 'Enter') {
+		submit_answer();
 	// if (e.keyCode === 13) {
-		btn_submit.click();
+		// btn_submit.click();
 		// txt_result.blur();
 		// alert(document.activeElement);
 	}
